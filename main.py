895dc58fefa0ab318a545acfbcb0f92cc48ccafa
bonnardadrien51 <58505025+bonnardadrien51@users.noreply.github.com>
2025-08-18 01:51:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ def main():
                 all_scores[participant]['scores'].setdefault(event_name, []).extend(score_list)
 
     solo_events = ['Garde les pieds sur terre', 'En avant les checkpoints', 'Vise la cible ou bien']
-    combined_event = 'Remontelapente a patte'
+    combined_event = 'Remonte la pente a patte'
     event_columns = solo_events + [combined_event]
 
     final_scores = []
